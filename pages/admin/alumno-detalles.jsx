66113d22e55886f4ca926ac57cc4d783ba7c8d8e
Kevin LG <luciano19940@hotmail.com>
2023-08-17 11:17:18 -0600

    Styles counter at student details

--- pages/admin/alumno-detalles.jsx
+++ pages/admin/alumno-detalles.jsx
@@ -33,90 +33,90 @@ const AlumnoDetallesPage = () => {
     fetchAlumno();
   }, [curp]);
 
-  const renderRespuestas = () => {
-    const patronesRespuestas = [
-      /* 1 */
-      ["B", "A", "C"],
-      /* 2 */
-      ["A", "C", "B"],
-      /* 3 */
-      ["B", "A", "C"],
-      /* 4 */
-      ["C", "B", "A"],
-      /* 5 */
-      ["C", "B", "A"],
-      /* 6 */
-      ["B", "A", "C"],
-      /* 7 */
-      ["A", "B", "C"],
-      /* 8 */
-      ["B", "A", "C"],
-      /* 9 */
-      ["A", "C", "B"],
-      /* 10 */
-      ["C", "B", "A"],
-      /* 11 */
-      ["B", "A", "C"],
-      /* 12 */
-      ["B", "C", "A"],
-      /* 13 */
-      ["C", "A", "B"],
-      /* 14 */
-      ["A", "B", "C"],
-      /* 15 */
-      ["B", "A", "C"],
-      /* 16 */
-      ["A", "C", "B"],
-      /* 17 */
-      ["C", "B", "A"],
-      /* 18 */
-      ["C", "A", "B"],
-      /* 19 */
-      ["A", "B", "C"],
-      /* 20 */
-      ["A", "C", "B"],
-      /* 21 */
-      ["B", "C", "A"],
-      /* 22 */
-      ["C", "A", "B"],
-      /* 23 */
-      ["A", "B", "C"],
-      /* 24 */
-      ["B", "A", "C"],
-      /* 25 */
-      ["A", "B", "C"],
-      /* 26 */
-      ["C", "B", "A"],
-      /* 27 */
-      ["B", "A", "C"],
-      /* 28 */
-      ["C", "B", "A"],
-      /* 29 */
-      ["B", "C", "A"],
-      /* 30 */
-      ["C", "B", "A"],
-      /* 31 */
-      ["B", "A", "C"],
-      /* 32 */
-      ["C", "A", "B"],
-      /* 33 */
-      ["A", "C", "B"],
-      /* 34 */
-      ["B", "A", "C"],
-      /* 35 */
-      ["B", "C", "A"],
-      /* 36 */
-      ["A", "C", "B"],
-      /* 37 */
-      ["A", "B", "C"],
-      /* 38 */
-      ["B", "C", "A"],
-      /* 39 */
-      ["B", "C", "A"],
-      /* 40 */
-      ["C", "A", "B"],
-    ];
+  const patronesRespuestas = [
+    /* 1 */
+    ["B", "A", "C"],
+    /* 2 */
+    ["A", "C", "B"],
+    /* 3 */
+    ["B", "A", "C"],
+    /* 4 */
+    ["C", "B", "A"],
+    /* 5 */
+    ["C", "B", "A"],
+    /* 6 */
+    ["B", "A", "C"],
+    /* 7 */
+    ["A", "B", "C"],
+    /* 8 */
+    ["B", "A", "C"],
+    /* 9 */
+    ["A", "C", "B"],
+    /* 10 */
+    ["C", "B", "A"],
+    /* 11 */
+    ["B", "A", "C"],
+    /* 12 */
+    ["B", "C", "A"],
+    /* 13 */
+    ["C", "A", "B"],
+    /* 14 */
+    ["A", "B", "C"],
+    /* 15 */
+    ["B", "A", "C"],
+    /* 16 */
+    ["A", "C", "B"],
+    /* 17 */
+    ["C", "B", "A"],
+    /* 18 */
+    ["C", "A", "B"],
+    /* 19 */
+    ["A", "B", "C"],
+    /* 20 */
+    ["A", "C", "B"],
+    /* 21 */
+    ["B", "C", "A"],
+    /* 22 */
+    ["C", "A", "B"],
+    /* 23 */
+    ["A", "B", "C"],
+    /* 24 */
+    ["B", "A", "C"],
+    /* 25 */
+    ["A", "B", "C"],
+    /* 26 */
+    ["C", "B", "A"],
+    /* 27 */
+    ["B", "A", "C"],
+    /* 28 */
+    ["C", "B", "A"],
+    /* 29 */
+    ["B", "C", "A"],
+    /* 30 */
+    ["C", "B", "A"],
+    /* 31 */
+    ["B", "A", "C"],
+    /* 32 */
+    ["C", "A", "B"],
+    /* 33 */
+    ["A", "C", "B"],
+    /* 34 */
+    ["B", "A", "C"],
+    /* 35 */
+    ["B", "C", "A"],
+    /* 36 */
+    ["A", "C", "B"],
+    /* 37 */
+    ["A", "B", "C"],
+    /* 38 */
+    ["B", "C", "A"],
+    /* 39 */
+    ["B", "C", "A"],
+    /* 40 */
+    ["C", "A", "B"],
+  ];
 
+  const renderRespuestas = () => {
     if (!alumno) return null;
 
     const respuestasAlumno = [];
@@ -137,6 +137,18 @@ const AlumnoDetallesPage = () => {
       );
     }
     return respuestasAlumno;
+  };
+
+  const countEstilo = (estilo) => {
+    if (!alumno) return 0;
+
+    let count = 0;
+    for (let i = 0; i < patronesRespuestas.length; i++) {
+      if (alumno[`element${i + 1}`] === patronesRespuestas[i][estilo]) {
+        count++;
+      }
+    }
+    return count;
   };
 
   // Define los estilos del documento PDF
@@ -179,7 +191,7 @@ const AlumnoDetallesPage = () => {
         {alumno && (
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
             <div className="bg-white p-4 rounded-lg shadow">
-              <p className="text-xl font-semibold mb-2">
+              <p className="text-2xl font-semibold mb-4 text-center">
                 Información del Alumno
               </p>
               <p>
@@ -197,7 +209,7 @@ const AlumnoDetallesPage = () => {
               </p>
             </div>
             <div className="bg-white p-4 rounded-lg shadow">
-              <p className="text-xl font-semibold mb-2">
+              <p className="text-2xl font-semibold mb-4 text-center">
                 Respuestas del Alumno
               </p>
               <Table aria-label="Tabla de Respuestas">
@@ -217,6 +229,24 @@ const AlumnoDetallesPage = () => {
                 </TableHeader>
                 <TableBody>{renderRespuestas()}</TableBody>
               </Table>
+              {/* Contador de estilos */}
+              <div className="flex justify-center mt-4">
+                <div className="mr-4">
+                  <p className="text-lg font-semibold">
+                    Visual: {countEstilo(0)}
+                  </p>
+                </div>
+                <div className="mr-4">
+                  <p className="text-lg font-semibold">
+                    Auditivo: {countEstilo(1)}
+                  </p>
+                </div>
+                <div>
+                  <p className="text-lg font-semibold">
+                    Kinestésico: {countEstilo(2)}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         )}
