bb10fd9f5b0adfb40b81d62db38a053b9f52a1fc
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-08-23 01:47:01 -0600

    Test page for test

---
+++ pages/resultados/test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutPublic from "./LayoutPublic";
+
+const test = () => {
+  return (
+    <LayoutPublic>
+      <div>test</div>
+    </LayoutPublic>
+  );
+};
+
+export default test;
