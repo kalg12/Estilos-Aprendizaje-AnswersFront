0c7884313c54c5c113a754b474a7ad9f93151bc7
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-08-23 12:29:13 -0600

    Page example

---
+++ pages/resultados/lalo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutPublic from "./LayoutPublic";
+LayoutPublic;
+
+const lalo = () => {
+  return (
+    <LayoutPublic>
+      <div>lalo</div>
+    </LayoutPublic>
+  );
+};
+
+export default lalo;
