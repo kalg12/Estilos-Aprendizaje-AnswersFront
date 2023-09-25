0fae04eac3803360f7a510b5eb82ea6eafdcaa4a
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-09-25 00:17:13 -0600

    Revert "[ADDED] instructions"
    This reverts commit 42c432e2d2632b04511e42a2bf3bfafecb6d4d45.

--- pages/admin/all.jsx
+++ pages/admin/all.jsx
@@ -62,7 +62,7 @@ const AllPage = () => {
     worksheet.getColumn("B").width = 35; // Ancho para Nombre
 
     // Encabezados
-    const headers = ["ID", "Nombres", "Grupo", "Estilo de Aprendizaje"]; // Agregado de estilo de aprendizaje
+    const headers = ["ID", "Nombres", "Grupo", "Estilo de Aprendizaje"];
     for (let i = 1; i <= 40; i++) {
       headers.push(i.toString());
     }
